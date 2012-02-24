cf0aa755140ecbbabecf83ce94fb63099408c368
Tim Oxley <secoif@gmail.com>
2012-02-24 19:57:38 +1000

    redesign filter plugin to include optional blacklist

--- plugins/filter.js
+++ plugins/filter.js
@@ -1,15 +1,45 @@
-module.exports = function filter(whitelist) {
-  return function filter(schema) {
-    schema.method('filter', function(callback) {
-      var result = {}
-      for (var i = 0; i < whitelist.length; i++) {
-        var key = whitelist[i]
-        result[key] = this[key]
-      }
-      return result
-    })
-    schema.static('whitelist', function() {
-      return whitelist
-    })
+var _ = require('underscore')
+
+module.exports = {
+  in: function (whitelist) {
+    return function filter(schema) {
+      schema.method('filter', function(callback) {
+        var validProperties = JSON.parse(JSON.stringify(this))
+        var results = Object.create(null)
+        _.each(validProperties, function(value, key) {
+          if (_.include(whitelist, key)) {
+            results[key] = value
+          }
+        })
+        return results
+      })
+      schema.static('whitelist', function() {
+        return whitelist
+      })
+      schema.static('blacklist', function() {
+        return null
+      })
+    }
+  },
+  out: function (blacklist) {
+    return function filter(schema) {
+      schema.method('filter', function(callback) {
+        var validProperties = JSON.parse(JSON.stringify(this))
+        var results = Object.create(null)
+        _.each(validProperties, function(value, key) {
+          if (!_.include(blacklist, key)) {
+            results[key] = value
+          }
+        })
+        return results
+      })
+      schema.static('whitelist', function() {
+        return null
+      })
+      schema.static('blacklist', function() {
+        return blacklist
+      })
+    }
   }
+
 }
--- test/plugins/filter_test.js
+++ test/plugins/filter_test.js
@@ -15,13 +15,10 @@ var Helper = require('../../helpers/index')
 var helper = new Helper(mongoose)
 
 describe('filter plugin', function() {
-  var DummyModel
-  var whitelist = [
-    'publicProp1',
-    'publicProp2',
-    'publicProp3',
-    'publicProp4'
-  ]
+  var DummyModel, dummySchema
+  var whitelist, blacklist
+
+
   before(function(done) {
     mongoose.connect('mongodb://localhost/mongoose-toolbox')
     helper.dropCollections(done)
@@ -29,8 +26,20 @@ describe('filter plugin', function() {
   after(function(done) {
     helper.dropCollections(done)
   })
-  before(function() {
-    var dummySchema = new Schema({
+  beforeEach(function() {
+    whitelist = [
+      'publicProp1',
+      'publicProp2',
+      'publicProp3',
+      'publicProp4'
+    ]
+    blacklist = [
+      'privateProp1',
+      'privateProp2',
+      'privateProp3',
+      'privateProp4'
+    ]
+    dummySchema = new Schema({
       publicProp1: String,
       publicProp2: String,
       publicProp3: String,
@@ -40,25 +49,55 @@ describe('filter plugin', function() {
       privateProp3: String,
       privateProp4: String
     })
-    dummySchema.plugin(filter(whitelist))
-    DummyModel = mongoose.model('DummyMoodel', dummySchema)
   })
-  it('can get a list of whitelisted properties', function() {
-    assert.equal(DummyModel.whitelist(), whitelist)
-  })
-  it('will filter out private properties', function() {
-    var dummy = new DummyModel({
-      publicProp1: Faker.Lorem.words().pop(),
-      publicProp2: Faker.Lorem.words().pop(),
-      publicProp3: Faker.Lorem.words().pop(),
-      publicProp4: Faker.Lorem.words().pop(),
-      privateProp1: Faker.Lorem.words().pop(),
-      privateProp2: Faker.Lorem.words().pop(),
-      privateProp3: Faker.Lorem.words().pop(),
-      privateProp4: Faker.Lorem.words().pop()
+  describe('whitelist', function() {
+    beforeEach(function() {
+      dummySchema.plugin(filter.in(whitelist))
+      DummyModel = mongoose.model('DummyModel', dummySchema)
     })
-    assert.deepEqual(_.keys(dummy.filter()), whitelist)
+    it('can get a list of whitelisted properties', function() {
+      assert.deepEqual(DummyModel.whitelist(), whitelist)
+      assert.strictEqual(DummyModel.blacklist(), null)
+    })
+    it('will filter out private properties', function() {
+      var dummy = new DummyModel({
+        publicProp1: Faker.Lorem.words().pop(),
+        publicProp2: Faker.Lorem.words().pop(),
+        publicProp3: Faker.Lorem.words().pop(),
+        publicProp4: Faker.Lorem.words().pop(),
+        privateProp1: Faker.Lorem.words().pop(),
+        privateProp2: Faker.Lorem.words().pop(),
+        privateProp3: Faker.Lorem.words().pop(),
+        privateProp4: Faker.Lorem.words().pop()
+      })
+      assert.deepEqual(_.keys(dummy.filter()), whitelist)
+    })
   })
+  describe('blacklist', function() {
+    before(function() {
+      dummySchema.plugin(filter.out(blacklist))
+      DummyModel = mongoose.model('DummyModel2', dummySchema)
+    })
+    it('can get a list of filtered properties', function() {
+      assert.deepEqual(DummyModel.blacklist(), blacklist)
+      assert.strictEqual(DummyModel.whitelist(), null)
+    })
+    it('will filter out private properties', function() {
+      var dummy = new DummyModel({
+        publicProp1: Faker.Lorem.words().pop(),
+        publicProp2: Faker.Lorem.words().pop(),
+        publicProp3: Faker.Lorem.words().pop(),
+        publicProp4: Faker.Lorem.words().pop(),
+        privateProp1: Faker.Lorem.words().pop(),
+        privateProp2: Faker.Lorem.words().pop(),
+        privateProp3: Faker.Lorem.words().pop(),
+        privateProp4: Faker.Lorem.words().pop()
+      })
+      whitelist.push('_id')
+      assert.deepEqual(_.keys(dummy.filter()), whitelist)
+    })
+  })
+
 })
 
 
